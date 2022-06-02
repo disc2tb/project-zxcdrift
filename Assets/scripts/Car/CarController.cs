7d69fab0f27573d112e45824a74cbdd02aaa7497
cafeed28 <artlekseev@gmail.com>
2022-06-02 17:36:39 +0500

    yes i love racing i am racist

---
+++ Assets/scripts/Car/CarController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private Rigidbody _body;
+    public WheelController[] wheels;
+
+    private float _throttle;
+    private float _brake;
+    private float _handbrake;
+
+    private float _steering;
+    public float maxSteering = 35;
+
+    private void Start()
+    {
+        _body = GetComponent<Rigidbody>();
+
+        foreach (WheelController wheel in wheels)
+        {
+            wheel.Setup(_body);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        // _throttle = Input.GetAxis("Throttle");
+        // _steering = Input.GetAxis("Steering");
+        // _brake = Input.GetAxis("Brake");
+        // _handbrake = Input.GetAxisRaw("Handbrake");
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            wheels[i].transform.localEulerAngles = new Vector3(0, _steering * maxSteering, 0);
+        }
+
+        foreach (WheelController wheel in wheels)
+        {
+            wheel.Step(_throttle, _brake, _handbrake);
+        }
+    }
+}
---
+++ Assets/scripts/Car/WheelController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    private Rigidbody _body;
+
+    [Header("Suspesion")]
+    public float springRestLength = 0.5f;
+    public float springTravel = 0.2f;
+    public float springStiffness = 30000;
+    public float damperStiffness = 4000;
+
+    private float _length;
+    private float _lastLength;
+    private float _minLength;
+    private float _maxLength;
+
+    private float _springForce;
+    private float _springVelocity;
+    private float _damperForce;
+    private Vector3 _suspensionForce;
+
+    [Header("Wheel")]
+    public float radius = 0.34f;
+
+    public void Setup(Rigidbody body)
+    {
+        _body = body;
+        _minLength = springRestLength - springTravel;
+        _maxLength = springRestLength + springTravel;
+    }
+
+    public void Step(float throttle, float brake, float handbrake)
+    {
+        Raycast();
+    }
+
+    private void Raycast()
+    {
+        if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, _maxLength + radius))
+        {
+            _lastLength = _length;
+            _length = Mathf.Clamp(hit.distance - radius, _minLength, _maxLength);
+            _springVelocity = (_lastLength - _length) / Time.fixedDeltaTime;
+            _springForce = springStiffness * (springRestLength - _length);
+
+            _damperForce = damperStiffness * _springVelocity;
+
+            _suspensionForce = (_springForce + _damperForce) * transform.up;
+
+            _body.AddForceAtPosition(_suspensionForce, transform.position);
+        }
+        else
+        {
+            _length = _maxLength;
+        }
+
+        Debug.DrawRay(transform.position, -transform.up * (_length + radius), Color.magenta);
+    }
+}
