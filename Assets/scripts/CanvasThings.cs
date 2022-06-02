2689ee9a84efe2c13cebb557ed3b48af3d0c00d6
cafeed28 <artlekseev@gmail.com>
2022-06-02 17:50:21 +0500

    <:overdrive_shakal:941206395246751784>

--- Assets/scripts/CanvasThings.cs
+++ Assets/scripts/CanvasThings.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CanvasThings : MonoBehaviour
-{a
+{
     [Header("Render")]
     public RectTransform text1;
     public RectTransform text2;
@@ -11,6 +11,6 @@ public class CanvasThings : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
