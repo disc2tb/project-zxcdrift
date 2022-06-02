16e799a60a9e5b384c9e01ef22f7dbc64e48f935
arbuzplaya <dbebramaaan@gmail.com>
2022-06-02 19:44:27 +0700

    wfaf

---
+++ Assets/scripts/CanvasThings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasThings : MonoBehaviour
+{a
+    [Header("Render")]
+    public RectTransform text1;
+    public RectTransform text2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
