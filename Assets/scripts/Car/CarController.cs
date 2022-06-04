a19c52b6fb810a8129440d8a3ec8289c294dc902
cafeed28 <artlekseev@gmail.com>
2022-06-04 20:43:26 +0500

    а блять я же даунфорс удалил оно не работает

--- Assets/scripts/Car/CarController.cs
+++ Assets/scripts/Car/CarController.cs
@@ -46,8 +46,6 @@ public class CarController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // DownForce();
-
         for (int i = 0; i < 2; i++)
         {
             wheels[i].transform.localEulerAngles = new Vector3(0, _steering * maxSteering, 0);
@@ -57,34 +55,5 @@ public class CarController : MonoBehaviour
         {
             wheel.Step(_throttle);
         }
-    }
-
-    private void DownForce()
-    {
-        Vector3 frontAxis = (wheels[0].transform.position + wheels[1].transform.position) / 2;
-        Vector3 rearAxis = (wheels[3].transform.position + wheels[2].transform.position) / 2;
-
-        _downForce = 0;
-
-        for (int i = 0; i < 2; i++)
-        {
-            Vector3 velocityVector;
-
-            if (i == 0)
-                velocityVector = _body.GetPointVelocity(frontAxis);
-            else
-                velocityVector = _body.GetPointVelocity(rearAxis);
-
-            float velocity = Mathf.Max((Quaternion.Inverse(_body.transform.rotation) * velocityVector).z, 0);
-
-            float downForce = 0.5f * 1.22f * Mathf.Sqrt(velocity) * 5.0f / 2;
-            downForce *= 100; // N to Unity force
-            _downForce += downForce;
-
-            _body.AddForceAtPosition(-transform.up * downForce, velocityVector);
-        }
-
-        _downForceKG = _downForce / 9.8f;
-        _downForceToCarMass = _downForceKG / _body.mass;
     }
 }
